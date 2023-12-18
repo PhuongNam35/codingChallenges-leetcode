48e969cb0488dfa8a98af3577bac661a6cec1eda
PhuongNam35 <phuongnamacet@gmail.com>
2023-12-18 15:58:11 +0700

    bestProfit

--- BestTimeToBuyAndSellStock.py
+++ BestTimeToBuyAndSellStock.py
@@ -1,8 +1,10 @@
-from typing import List  #You are given an array prices where prices[i] is the price of a given stock on the ith day.
+    #You are given an array prices where prices[i] is the price of a given stock on the ith day.
     #You want to maximize your profit by choosing a single day to buy one stock and choosing a
 # different day in the future to sell that stock.
     #Return the maximum profit you can achieve from this transaction.
 # If you cannot achieve any profit, return 0.
+
+from typing import List
 
 #Using this function causes a Time limit Exceeded error (due to 2 for loops)
 class Solution:
@@ -16,13 +18,13 @@ class Solution:
         return max_profit
 
     def maxProfit_2(self, prices: List[int]) -> int:
+        max_profit = 0
         left = 0
         right = 1
-        max_profit = 0
         while right < len(prices):
-            current_profit = prices[right] - prices[left]
-            if prices[left] < prices[right]:
-                max_profit = max(current_profit, max_profit)
+            if prices[right] > prices[left]:
+                current_profit = prices[right] - prices[left]
+                max_profit = max(max_profit, current_profit)
             else:
                 left = right
             right += 1
